0243daac7f305876cf1f5141b7e57d94f8a478e5
Alexis Panousis <apanousis@gmail.com>
2023-05-16 14:41:36 +0300

    Used more functional style

--- src/main/java/com/agileactors/Main.java
+++ src/main/java/com/agileactors/Main.java
@@ -1,73 +1,113 @@
 package com.agileactors;
 
-import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-// Mimics a service that integrates with a third party and loads data <capacity> times.
+/**
+ * Mimics a service that integrates with a third party api and loads data <numberOfCallableCommands> times.
+ * Use JDK 21 EA (SDKMAN: sdk use java 21.ea.21-open)
+ */
 public class Main {
 
-    // Create a list with Callable commands
-    private static final int capacity = 5;
-    private static final List<Callable<String>> getDataCommands = new ArrayList<>(capacity);
-
-    static {
-        int i = 1;
-        while (i < capacity) {
-            int finalI = i++;
-            getDataCommands.add(() -> {
-                getRemoteData();
-                return "command " + finalI;
-            });
-        }
-    }
+    // Number of Callable commands
+    private static final int numberOfCallableCommands = 10;
 
     public static void main(String[] args) {
+        printAvailableProcessors();
         loadDataUsingNormalLoop();
         loadDataUsingOsThreadsLoop();
+        loadIngDataUsingParallelStream();
         loadDataUsingVirtualThreadsLoop();
     }
 
-    private static void loadDataUsingVirtualThreadsLoop() {
-        var start = System.currentTimeMillis();
-        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            executor.invokeAll(getDataCommands);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    private static void printAvailableProcessors() {
+        System.out.println("Available processors [" + Runtime.getRuntime().availableProcessors() + "]");
+    }
 
-        System.out.println("virtualThreadsLoop took " + (System.currentTimeMillis() - start) + " millis");
+    private static void loadDataUsingVirtualThreadsLoop() {
+        executeWithLogsAndMetrics("loadDataUsingVirtualThreadsLoop", (callableCommands) -> {
+            try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                executor.invokeAll(callableCommands.toList());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        });
     }
 
     private static void loadDataUsingOsThreadsLoop() {
-        var start = System.currentTimeMillis();
-        try (ExecutorService executor = Executors.newFixedThreadPool(16)) {
-            executor.invokeAll(getDataCommands);
+        executeWithLogsAndMetrics("loadDataUsingOsThreadsLoop", (callableCommands) -> {
+            try (ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())) {
+                executor.invokeAll(callableCommands.toList());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        });
+    }
+
+    private static void loadIngDataUsingParallelStream() {
+        executeWithLogsAndMetrics("loadIngDataUsingParallelStream", (callableCommands) -> {
+            callableCommands.parallel()
+                    .forEach(action -> {
+                        try {
+                            action.call();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+            return null;
+        });
+    }
+
+    public static void loadDataUsingNormalLoop() {
+        executeWithLogsAndMetrics("loadDataUsingNormalLoop", (callableCommands) -> {
+            callableCommands.forEach(command -> {
+                try {
+                    command.call();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            return null;
+        });
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println("osThreadsLoop took " + (System.currentTimeMillis() - start) + " millis");
     }
 
-    private static void loadDataUsingNormalLoop() {
+    private static Stream<Callable<Object>> getCallableCommands() {
+        return IntStream.range(0, numberOfCallableCommands).mapToObj(Main::getRemoteData);
+    }
+
+    // Just invoke a remote api to get dummy data. This is a blocking statement
+    private static Callable<Object> getRemoteData(int i) {
+        return () -> {
+            var threadName = Optional.of(Thread.currentThread().getName().trim());
+            threadName.ifPresent(name -> System.out.println("Thread [" + name + "] runs [" + i + "]"));
+
+//            sleep();
+            return URI.create("https://run.mocky.io/v3/8b916b06-1f82-446c-9391-263f37058ffe?random=" + i).toURL().getContent();
+        };
+    }
+
+    public static void executeWithLogsAndMetrics(String tag, Function<Stream<Callable<Object>>, Void> function) {
+        var callableCommands = getCallableCommands();
+
+        System.out.println("\n" + tag + " started");
         var start = System.currentTimeMillis();
-        for (Callable<String> command : getDataCommands) {
-            try {
-                command.call();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        System.out.println("normalLoop took " + (System.currentTimeMillis() - start) + " millis");
-    }
-
-    // Just invoke a remote api to get dummy data
-    private static void getRemoteData() throws IOException {
-        URI.create("https://run.mocky.io/v3/8b916b06-1f82-446c-9391-263f37058ffe").toURL().getContent();
+        function.apply(callableCommands);
+        System.out.println(tag + " took " + (System.currentTimeMillis() - start) + " millis");
     }
 }
